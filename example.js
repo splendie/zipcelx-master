4e08e469efc29bd1bc04d70a0e7987d60f227f34
splendie <indri.yunita@ymail.com>
2019-09-09 12:01:11 +0700

    Add files via upload

---
+++ example.js
@@ -0,0 +1,23 @@
+import zipcelx from 'zipcelx';
+
+const config = {
+  filename: 'general-ledger-Q1',
+  sheet: {
+    data: [
+      [
+        {
+          value: 'Income - Webshop',
+          type: 'string',
+        },
+        {
+          value: 1000,
+          type: 'number',
+        },
+      ],
+    ],
+  },
+};
+
+export default () => {
+  zipcelx(config);
+};
